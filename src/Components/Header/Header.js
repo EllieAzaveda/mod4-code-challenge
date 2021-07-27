85a63dc1e7df2cdc5841b0c5df8c381288d24d02
EllieAzaveda <elliemiller66@icloud.com>
2021-07-27 11:49:26 -0600

    Add Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <nav className='header'>
+      <h1 className='header-title'>NY Times Top Stories</h1>
+    </nav>
+  )
+}
+
+export default Header;
