9eb37d64e3d89da54007526a300cb949f68b1380
EllieAzaveda <elliemiller66@icloud.com>
2021-07-28 00:03:38 -0600

    Add class names

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -4,11 +4,13 @@ import './ArticleCard.css';
 
 const ArticleCard = ({ article }) => {
   return (
-    <NavLink to={`/article-details/${article.title}`} >
+    <NavLink to={`/article-details/${article.title}`} style={{ textDecoration: 'none' }} >
       <div className='card'>
-        <img className='article-image' src={article.multimedia[0].url} alt={article.multimedia[0].caption}/>
-        <h2 className='title'>{article.title}</h2>
-        <h2 className='author'>{article.byline}</h2>
+        <img className='preview-image' src={article.multimedia[0].url} alt={article.multimedia[0].caption}/>
+        <div className='title-container'>
+          <h2 className='title'>{article.title}</h2>
+          <h2 className='author'>{article.byline}</h2>
+        </div>
       </div>
     </NavLink>
   )
