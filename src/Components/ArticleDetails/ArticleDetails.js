e5e419e22974f86528747bdff97dd71e6249d107
EllieAzaveda <elliemiller66@icloud.com>
2021-07-28 11:19:52 -0600

    Fix date displayed in article details

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 import './ArticleDetails.css';
 
 class ArticleDetails extends Component {
@@ -10,6 +9,28 @@ class ArticleDetails extends Component {
       }
   }
 
+  formatDate = (date) => {
+    let months = {
+      "01": "January",
+      "02": "February",
+      "03": "March",
+      "04": "April",
+      "05": "May",
+      "06": "June",
+      "07": "July",
+      "08": "August",
+      "09": "September",
+      "10": "October",
+      "11": "November",
+      "12": "December"
+    };
+
+    let splitDate = date.split("T");
+    let dateOnly = splitDate[0].split("-");
+    let matchedMonth = months[dateOnly[1]];
+    return `${matchedMonth} ${dateOnly[2]}, ${dateOnly[0]}`;
+  }
+
   render() {
     return (
       <div className="details-container">
@@ -17,7 +38,7 @@ class ArticleDetails extends Component {
         <img className="article-image" src={this.state.article.multimedia[0].url} alt={this.state.article.multimedia[0].caption}/>
         <h2 className="title">{this.state.article.title}</h2>
         <h4 className="author">{this.state.article.byline}</h4>
-        <h5 className="date">{this.state.article.created_date}</h5>
+        <h5 className="date">{this.formatDate(this.state.article.created_date)}</h5>
         <p className="abstract">{this.state.article.abstract}</p>
         <button
           onClick={(e) => {
