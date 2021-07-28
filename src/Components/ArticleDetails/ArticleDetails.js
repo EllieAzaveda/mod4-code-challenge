94205613b08b42d277c861f5e6d1eacd5d7508ce
EllieAzaveda <elliemiller66@icloud.com>
2021-07-27 22:40:05 -0600

    Fix refresh bug

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './ArticleDetails.css';
 
-const ArticleDetails = ({ article }) => {
-  return (
-    <div className="article-details">
-      <img className="article-image" src={article.multimedia[0].url} alt={article.multimedia[0].caption}/>
-      <h2 className="title">{article.title}</h2>
-      <h4 className="author">{article.byline}</h4>
-      <h5 className="date">{article.created_date}</h5>
-      <p className="abstract">{article.abstract}</p>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          window.location.href=`${article.short_url}`;
-      }}>Read this article HERE</button>
-    </div>
-  )
+class ArticleDetails extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        article: props.article
+      }
+  }
+
+  render() {
+    return (
+      <>
+      <NavLink to='/'><button className='home-button'>Back to Home Page</button></NavLink>
+      <div className="article-details">
+        <img className="article-image" src={this.state.article.multimedia[0].url} alt={this.state.article.multimedia[0].caption}/>
+        <h2 className="title">{this.state.article.title}</h2>
+        <h4 className="author">{this.state.article.byline}</h4>
+        <h5 className="date">{this.state.article.created_date}</h5>
+        <p className="abstract">{this.state.article.abstract}</p>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href=`${this.state.article.short_url}`;
+        }}>Read this article HERE</button>
+      </div>
+      </>
+    )
+  }
 }
 
 export default ArticleDetails;
